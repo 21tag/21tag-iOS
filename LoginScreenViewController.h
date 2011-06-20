c8f6a7db7bd95fb4053e7faba21a17f1354e400a
Chris Ballinger <chrisballinger@gmail.com>
2011-06-19 21:42:39 -0400

    Add login screen

---
+++ LoginScreenViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginScreenViewController.h
+//  TwentyFirstCenturyTag
+//
+//  Created by Christopher Ballinger on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginScreenViewController : UIViewController {
+    
+}
+
+@end
