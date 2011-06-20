f147135d4bc0450fe74dc9dcbf437f1ce5cd7025
Chris Ballinger <chrisballinger@gmail.com>
2011-06-19 20:13:12 -0400

    Initial Commit

---
+++ TwentyFirstCenturyTag/TwentyFirstCenturyTagAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TwentyFirstCenturyTagAppDelegate.h
+//  TwentyFirstCenturyTag
+//
+//  Created by Christopher Ballinger on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TwentyFirstCenturyTagViewController;
+
+@interface TwentyFirstCenturyTagAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet TwentyFirstCenturyTagViewController *viewController;
+
+@end
---
+++ TwentyFirstCenturyTag/TwentyFirstCenturyTagViewController.h
@@ -0,0 +1,15 @@
+//
+//  TwentyFirstCenturyTagViewController.h
+//  TwentyFirstCenturyTag
+//
+//  Created by Christopher Ballinger on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwentyFirstCenturyTagViewController : UIViewController {
+    
+}
+
+@end
