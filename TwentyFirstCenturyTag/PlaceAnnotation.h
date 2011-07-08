c838966bf4eda630907294b27593b2e7909f4427
Chris Ballinger <chrisballinger@gmail.com>
2011-07-08 14:27:20 -0400

    fix copyright notice

--- TwentyFirstCenturyTag/PlaceAnnotation.h
+++ TwentyFirstCenturyTag/PlaceAnnotation.h
@@ -3,7 +3,7 @@
 //  TwentyFirstCenturyTag
 //
 //  Created by Christopher Ballinger on 6/18/11.
-//  Copyright 2011 . All rights reserved.
+//  Copyright 2011. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
